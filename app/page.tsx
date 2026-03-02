66b47052c480324b1a636052dbf7ac47e25bd526
carledwardm <cmillardv77@gmail.com>
2026-03-02 17:18:36 -0500

    Add link to signup component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       <h1>Component Library for React/Next.js</h1>
-       
+        <h1>Component Library for React/Next.js</h1>
+        <h2 className={styles.componentListTitle}>Component List</h2>
+        <ul className={styles.componentList}>
+          <li className={styles.componentLink}><Link href="/signup">Sign Up Component</ Link></li>
+        </ul>
       </main>
     </div>
   );
