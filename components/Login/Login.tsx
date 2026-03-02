7c10745e25eb4144b70d95e5667c859619d67ab5
carledwardm <cmillardv77@gmail.com>
2026-03-02 16:55:50 -0500

    Update login jsx/html

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import styles from "./Login.module.scss";
 
+
+
 export default function Login() {
+    const [ userEmail, setUserEmail ] = useState<string>("");
+    const [ userPassword, setUserPassword ] = useState<string>("");
+
+    const handleSubmit = async (e: React.SubmitEvent) => {
+        e.preventDefault();
+        // Submit logic goes here
+    }
+
     return (
-        <h1>Login</h1>
+        <section className={styles.loginSection}>
+                <h1 className={styles.loginTitle}>Log In</h1>
+                <form className={styles.loginForm} onSubmit={handleSubmit}>
+                    <input type="text" className={styles.input} placeholder="Email" onChange={ e => setUserEmail(e.target.value)}></input>
+                    <input type="password" className={styles.input} placeholder="Password" onChange={ e => setUserPassword(e.target.value)}></input>
+                    <button type="submit" className={styles.submitBtn}>Submit</button>
+                </form>
+                <p className={styles.signupText}>Need an account? <a href="/signup" className={styles.signupLink}>Sign up</a>!</p>
+            </section>
     )
 }
