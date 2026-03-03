a6b0dfaba8a52926f35398a8fca58e894b6f7044
carledwardm <cmillardv77@gmail.com>
2026-03-03 16:52:28 -0500

    Add home link to signup component page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Link from "next/link";
 import styles from "./Login.module.scss";
 import Login from "@/components/Login/Login";
 
 export default function LogIn() {
     return (
         <main className={styles.logInMain}>
+            <Link href="/" className={styles.homeLink}>Home</ Link>
             <Login />
         </main>
     )
