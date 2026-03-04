acb818d979b5d3d6ffab5ea33f23de7d53fd6fb1
carledwardm <cmillardv77@gmail.com>
2026-03-04 16:39:25 -0500

    Place component links in nav tag, increase margin between title and nav

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,15 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Component Library for React/Next.js</h1>
-        <h2 className={styles.componentListTitle}>Available Components</h2>
-        <ul className={styles.componentList}>
-          <li className={styles.componentLink}><Link href="/login">Log In Component</ Link></li>
-          <li className={styles.componentLink}><Link href="/signup">Sign Up Component</ Link></li>
-          <li className={styles.componentLink}><Link href="/toast">Toast Component</ Link></li>
-        </ul>
+        <h1 className={styles.libraryTitle}>Component Library for React/Next.js</h1>
+        <nav className={styles.componentNav}>
+          <h2 className={styles.componentListTitle}>Available Components</h2>
+          <ul className={styles.componentList}>
+            <li className={styles.componentLink}><Link href="/login">Log In Component</ Link></li>
+            <li className={styles.componentLink}><Link href="/signup">Sign Up Component</ Link></li>
+            <li className={styles.componentLink}><Link href="/toast">Toast Component</ Link></li>
+          </ul>
+        </nav>
       </main>
     </div>
   );
