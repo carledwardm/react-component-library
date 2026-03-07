bf274571c833e97d038c4ddfc56012acafec4da7
carledwardm <cmillardv77@gmail.com>
2026-03-07 10:46:52 -0500

    Add signup component to storybook, remove glow effects for now

---
+++ stories/Signup.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import { fn } from 'storybook/test';
+
+import Signup from '@/components/Signup/Signup';
+
+const meta = {
+    title: 'Signup',
+    component: Signup,
+    parameters: {
+        layout: 'padded',
+    }
+} satisfies Meta<typeof Signup>
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        primary: true,
+        label: 'Signup',
+    }
+}
