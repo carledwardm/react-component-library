29dbbda63fdec116c1609da96470a5d6be4ea624
carledwardm <cmillardv77@gmail.com>
2026-03-07 11:04:00 -0500

    Add toast to stories

---
+++ stories/Toast.stories.ts
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import { fn } from 'storybook/test';
+
+import Toast from '@/components/Toast/Toast';
+
+const meta = {
+    title: 'Toast',
+    component: Toast,
+    parameters: {
+        layout: 'centered',
+    }
+} satisfies Meta<typeof Toast>
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        message: "This is a default Toast message.",
+        onClose: fn(),
+        // fn() is a placeholder/test function, use your own for the live component, typically a setter function
+    }
+}
