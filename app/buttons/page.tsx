513262c7b10edd36c0ec47470da4eb0c6f09071b
carledwardm <cmillardv77@gmail.com>
2026-03-12 18:13:35 -0400

    Add route for button components, rework styling for breadcrumbs, add section container

---
+++ app/buttons/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import styles from "./Buttons.module.scss";
+import LoadMore from "@/components/Buttons/LoadMore";
+import { FaChevronRight } from "react-icons/fa";
+import { useState } from "react";
+
+export default function Header() {
+    const [ count, setCount ] = useState<number>(0);
+    
+    return (
+        <main className={styles.headerMain}>
+            <nav className={styles.breadcrumbs}>
+                    <Link href="/" className={styles.homeLink}>Home </ Link>
+                    <FaChevronRight className={styles.breadcrumbsChev} />
+                    <span className={styles.routeName}>Buttons</span>
+            </nav>  
+            <section className={styles.componentsSection}>
+                <h1 className={styles.pageTitle}>Button Components</h1>
+                <div className={styles.componentContainer}>
+                    <h2 className={styles.componentTitle}>Load More Button</h2>
+                    <LoadMore increaseFunction={setCount} increaseAmount={1} />
+                    <p className={styles.descriptionText}>An incremental counter button designed to take a setter function for a numerical
+                        count and an increase amount. It will call your supplied function when clicked - this component is meant for 
+                        conditional rendering of additional UI items originally hidden from view.
+                    </p>
+                
+                    <Link 
+                        className={styles.repoLink} 
+                        href="https://github.com/carledwardm/react-component-library/tree/main/components/Buttons/LoadMore"
+                        target="_blank">
+                            <FaChevronRight className={styles.repoChev}/> Repo Link
+                    </ Link>
+                </div>
+            </section>
+        </main>
+    )
+}
