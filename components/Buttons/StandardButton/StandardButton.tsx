5bf4e39e9d98245275f8c424dccb2033750e09e1
carledwardm <cmillardv77@gmail.com>
2026-03-16 16:16:52 -0400

    Add Standard Button to storybook, add primary argument to button and type

--- components/Buttons/StandardButton/StandardButton.tsx
+++ components/Buttons/StandardButton/StandardButton.tsx
@@ -1,10 +1,11 @@
 import styles from "./StandardButton.module.scss";
 
 type StandardButtonProps = {
+    primary: boolean;
     text?: string;
 }
 
-export default function StandardButton({ text="Default text"} : StandardButtonProps) {
+export default function StandardButton({ text="Default text", primary=false} : StandardButtonProps) {
     return (
         <button className={styles.button}>{text}</button>
     )
---
+++ stories/StandardButton.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+
+import { fn } from 'storybook/test';
+
+import StandardButton from '@/components/Buttons/StandardButton';
+
+const meta = {
+    title: 'Standard Button',
+    component: StandardButton,
+    parameters: {
+        layout: 'padded',
+    }
+} satisfies Meta<typeof StandardButton>
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        primary: true,
+    }
+}
