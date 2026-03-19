b7bbb536d03f2b89d89dfdc0a1375da7c6114887
carledwardm <cmillardv77@gmail.com>
2026-03-19 18:09:29 -0400

    Add section container and h1 to signup and toast routes

--- app/toast/page.tsx
+++ app/toast/page.tsx
@@ -13,21 +13,25 @@ export default function SignUp() {
 
     return (
         <main className="main">
-            <div className={styles.componentContainer}>    
-                <div className={styles.componentDisplay}>       
-                    <Toast message="Your toast message will go here" onClose={closeFunction} />
-                </div>  
-                <p className={styles.descriptionText}>A component that's used to display feedback messages to your user. Accepts a message, duration and 
-                    a closing function. The fixed position parameter has been commented out for this demo, uncomment and
-                    adjust to your liking.
-                </p>
-                <Link 
-                    className={styles.repoLink} 
-                    href="https://github.com/carledwardm/react-component-library/tree/main/components/Toast"
-                    target="_blank">
-                        <FaChevronRight className={styles.repoChev}/> Repo Link
-                </ Link>
-            </div>
+            <section className={styles.componentsSection}>
+                <h1 className={styles.pageTitle}>Toast Components</h1>
+                <hr className={styles.divider} />
+                <div className={styles.componentContainer}>    
+                    <div className={styles.componentDisplay}>       
+                        <Toast message="Your toast message will go here" onClose={closeFunction} />
+                    </div>  
+                    <p className={styles.descriptionText}>A component that's used to display feedback messages to your user. Accepts a message, duration and 
+                        a closing function. The fixed position parameter has been commented out for this demo, uncomment and
+                        adjust to your liking.
+                    </p>
+                    <Link 
+                        className={styles.repoLink} 
+                        href="https://github.com/carledwardm/react-component-library/tree/main/components/Toast"
+                        target="_blank">
+                            <FaChevronRight className={styles.repoChev}/> Repo Link
+                    </ Link>
+                </div>
+            </section>
         </main>
     )
 }
