678ee0a32a16b582072fd05e215ea03f9c04fc0e
carledwardm <cmillardv77@gmail.com>
2026-03-24 18:22:33 -0400

    Move homepage nav to left sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,8 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.homeMain}>
-        <div className={styles.libraryTextContainer}>
-          <h1 className={styles.libraryTitle}>Component Library for React/Next.js</h1>
-          <p className={styles.libraryGreeting}>Hello! Welcome to my componeny library! Feel free to check out the available components and copy any code
-            you'd like to use from them via the repo links on each page. Check back in the future for updates!
-          </p>
-        
-        <nav className={styles.componentNav}>
+        <div className={styles.leftSideBar}>
+          <nav className={styles.componentNav}>
           <h2 className={styles.componentListTitle}>Available Components</h2>
           <ul className={styles.componentList}>
             <li className={styles.componentLink}><Link href="/buttons"><FaChevronRight className={styles.componentChev}/> Buttons</ Link></li>
@@ -24,6 +19,12 @@ export default function Home() {
             <li className={styles.componentLink}><Link href="/toast"><FaChevronRight className={styles.componentChev}/> Toast</ Link></li>
           </ul>
         </nav>
+        </div>
+        <div className={styles.libraryTextContainer}>
+          <h1 className={styles.libraryTitle}>Component Library for React/Next.js</h1>
+          <p className={styles.libraryGreeting}>Hello! Welcome to my componeny library! Feel free to check out the available components and copy any code
+            you'd like to use from them via the repo links on each page. Check back in the future for updates!
+          </p>
           <Link 
             className={styles.repoLink} 
             href="https://github.com/carledwardm/react-component-library/tree/main"
