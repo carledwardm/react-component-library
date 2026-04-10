44763b13dd02c0c714c76bc06a564374390cbe73
carledwardm <cmillardv77@gmail.com>
2026-04-10 16:59:20 -0400

    Add basic styling height/width/bg to highLighter component

--- appComponents/CodeBlock/CodeBlock.tsx
+++ appComponents/CodeBlock/CodeBlock.tsx
@@ -2,9 +2,9 @@ import styles from "./CodeBlock.module.scss";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-export default function CodeBlock(codeString: string) {
+export default function CodeBlock({codeString}: {codeString: string}) {
     return (
-        <SyntaxHighlighter language="jsx" style={dark}>
+        <SyntaxHighlighter language="jsx" style={dark} className={styles.highlighter}>
             {codeString}
         </SyntaxHighlighter>
     )
